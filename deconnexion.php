98219238457c7f7bebd01acc36e25a3e3420bfc2
TWellan <80897970+TWellan@users.noreply.github.com>
2021-03-31 17:40:21 +0200

    connexion
    Changement de retour à la page

--- deconnexion.php
+++ deconnexion.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
     session_destroy(); //suppression des cookies
-    include("inscription.php"); //retour à la page inscription
+    include("connexion.php"); //retour à la page connexion
 ?>
